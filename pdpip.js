61a38b2c3bf025eb1571e0e392670678e896f481
Quaker <bigboyq@gmail.com>
2022-03-07 16:51:59 +0800

    Add ; after each Line

--- pdpip.js
+++ pdpip.js
@@ -1,3 +1,3 @@
-let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface
-$httpClient.get("https://hk119.ensecure.info/"+mesg)
-$done()
+let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface;
+$httpClient.get("https://hk119.ensecure.info/"+mesg);
+$done();
