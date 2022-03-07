2daa62d0ff47913efddfd550677a73a12799ca3a
Quaker <bigboyq@gmail.com>
2022-03-07 19:41:21 +0800

    await

--- pdpip.js
+++ pdpip.js
@@ -1,8 +1,8 @@
-if (await !$network.v4.primaryAddress) {
-	console.log("Network Null");
-	}else{
-	let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface;
-	console.log("LAN:"+mesg);
+let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface;
+if (await $network.v4.primaryAddress) {
 	$httpClient.get("https://hk119.ensecure.info/"+mesg);
+	console.log("LAN:"+mesg);
+	}else{
+	console.log("Network Null");
 	}
 $done();
