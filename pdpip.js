b3dd3263ae005cd152a6dd28b68e2cb0879c2379
Quaker <bigboyq@gmail.com>
2022-03-07 17:22:19 +0800

    log

--- pdpip.js
+++ pdpip.js
@@ -1,11 +1,4 @@
 let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface;
-
-sleep(5000)
-	.then(()=> $httpClient.get("https://hk119.ensecure.info/"+mesg));
-
+console.log(mesg);
+$httpClient.get("https://hk119.ensecure.info/"+mesg);
 $done();
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
