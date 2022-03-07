bfff9bfc0cd9997ca0e90e0a0221f6b2d5535a08
Quaker <bigboyq@gmail.com>
2022-03-07 17:18:05 +0800

    sleep implementation

--- pdpip.js
+++ pdpip.js
@@ -1,5 +1,11 @@
-const delay = ms => new Promise(res => setTimeout(res, ms));
 let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface;
-await delay(5000);
-$httpClient.get("https://hk119.ensecure.info/"+mesg);
+
+sleep(5000)
+	.then(()=> $httpClient.get("https://hk119.ensecure.info/"+mesg));
+
 $done();
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
