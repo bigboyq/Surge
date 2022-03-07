e2be8f43235aae9648e692174a420eeee27cc3d4
Quaker <bigboyq@gmail.com>
2022-03-07 17:28:46 +0800

    LAN/WAN

--- pdpip.js
+++ pdpip.js
@@ -1,4 +1,4 @@
 let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface;
-console.log("AAA:"+mesg);
-$httpClient.get("https://hk119.ensecure.info/"+mesg);
+console.log("LOCAL:"+mesg);
+console.log("IP:"+$httpClient.get("https://hk119.ensecure.info/"+mesg));
 $done();
