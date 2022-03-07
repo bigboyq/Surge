e092233fe35819639a3fb96848e833173d145e82
Quaker <bigboyq@gmail.com>
2022-03-07 19:29:23 +0800

    IF else

--- pdpip.js
+++ pdpip.js
@@ -1,8 +1,8 @@
 if (!$network.v4.primaryAddress) {
 	console.log("Network Null");
-	$done();
-}
-let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface;
-console.log("LAN:"+mesg);
-$httpClient.get("https://hk119.ensecure.info/"+mesg);
+	}else{
+	let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface;
+	console.log("LAN:"+mesg);
+	$httpClient.get("https://hk119.ensecure.info/"+mesg);
+	}
 $done();
