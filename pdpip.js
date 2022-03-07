c59c1e68b9f65d75601e92680669e515f2e85d89
Quaker <bigboyq@gmail.com>
2022-03-07 19:42:55 +0800

    Remove await

--- pdpip.js
+++ pdpip.js
@@ -1,5 +1,5 @@
 let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface;
-if (await $network.v4.primaryAddress) {
+if ($network.v4.primaryAddress) {
 	$httpClient.get("https://hk119.ensecure.info/"+mesg);
 	console.log("LAN:"+mesg);
 	}else{
