6c0ca4a8f51ef34089c2d879db951861394d9b64
Quaker <bigboyq@gmail.com>
2022-03-07 17:38:15 +0800

    content

--- pdpip.js
+++ pdpip.js
@@ -1,4 +1,4 @@
 let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface;
 console.log("LAN:"+mesg);
-$httpClient.get("https://hk119.ensecure.info/"+mesg, func(resp){ console.log("WAN:"+resp);});
+console.log("WAN:"+$httpClient.get("https://hk119.ensecure.info/"+mesg).getResponse().content);
 $done();
