2d6249f789d82b1fa5a4f672976a6bb95354ce48
Quaker <bigboyq@gmail.com>
2022-03-07 19:25:37 +0800

    Null Detect

--- pdpip.js
+++ pdpip.js
@@ -1,3 +1,7 @@
+if (!$network.v4.primaryAddress) {
+	console.log("Network Null");
+	$done();
+}
 let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface;
 console.log("LAN:"+mesg);
 $httpClient.get("https://hk119.ensecure.info/"+mesg);
