da335d71b75a75ff7fe0ba6c3eba1bf93873ccd6
Quaker <bigboyq@gmail.com>
2022-03-08 09:53:58 +0800

    OnComplete

--- pdpip.js
+++ pdpip.js
@@ -1,7 +1,14 @@
+function onComplete(response,error) {
+    if (response) {
+      console.log('HttpClient:'+response.status);
+     } else {
+      console.log('HttpClient: Error');
+     }
+}
 let mesg = $network.v4.primaryAddress + "_" + $network.v4.primaryInterface;
 console.log("Network Changed!");
 if ($network.v4.primaryAddress) {
-	$httpClient.get("https://hk119.ensecure.info/"+mesg);
+	var resp=$httpClient.get("https://hk119.ensecure.info/"+mesg,onComplete);	
 	console.log("LAN:"+mesg);
 	}
 $done();
